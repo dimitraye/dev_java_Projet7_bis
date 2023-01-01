ceb5fbfc4bc8774b7e1b2816ee2b8da466d9800d
dimitraye <dimitraye94@gmail.com>
2023-01-01 12:20:02 +0100

    RuleName CRUD has been made

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -3,7 +3,13 @@ package com.nnk.springboot.domain;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "rulename")
 public class RuleName {
@@ -12,4 +18,10 @@ public class RuleName {
   @Id
   @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Integer id;
+  String name;
+  String description;
+  String json;
+  String template;
+  String sqlStr;
+  String sqlPart;
 }
